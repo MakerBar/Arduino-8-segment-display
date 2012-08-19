6dc2e915ac3ed5c3b82750252c3fd2f9d7ce8ce2
Bert Hartmann <BertHartm@gmail.com>
2012-08-19 15:53:01 -0400

    make pin assignments changable

--- _8_seg_display.ino
+++ _8_seg_display.ino
@@ -1,6 +1,11 @@
 // handles a multiplexed 8 segment display
-// assumes digit selection for the 8-seg starts on pin 0
-// assumes segment selection starts on pin 4
+// ENSURE digitPin, segmentPin, AND ledPin ARE SET CORRECTLY!
+// there should be 4 digit pins (increasing from digitPin) and 7 segment pins
+// to control colon leds, float digits middle 2 digits HIGH, and set ledPin (labeled "dots") low
+
+int digitPin = 2;
+int segmentPin = 7;
+//int ledPin = 11;
 
 int digits[10][7] = {
   {1,1,1,1,1,1,0}, // 0
@@ -23,18 +28,13 @@ void setup() {
 }
 
 void reset() {
-  digitalWrite(0, LOW);
-  digitalWrite(1, LOW);
-  digitalWrite(2, LOW);
-  digitalWrite(3, LOW);
+  for (int p = digitPin; p < digitPin + 4; p++) {
+    digitalWrite(p, LOW);
+  }
   
-  digitalWrite(4, HIGH);
-  digitalWrite(5, HIGH);
-  digitalWrite(6, HIGH);
-  digitalWrite(7, HIGH);
-  digitalWrite(8, HIGH);
-  digitalWrite(9, HIGH);
-  digitalWrite(10, HIGH);
+  for (int p = segmentPin; p < segmentPin + 7; p++) {
+    digitalWrite(p, HIGH);
+  }
 }
 
 void fail() {
@@ -49,8 +49,8 @@ void lightSeg(int digit, int segment) {
   if ((digit < 1) || (digit > 4) || (segment < 0) || (segment > 6)) {
     fail();
   }
-  int digPin = digit - 1; // pins 0 - 3
-  int segPin = segment + 4; // pins 4 - 10
+  int digPin = digit - 1 + digitPin; // digits are 1-indexed, so subtract 1
+  int segPin = segment + segmentPin; // segments are 0 indexed
   digitalWrite(digPin, HIGH);
   digitalWrite(segPin, LOW);
   digitalWrite(digPin, LOW);
@@ -79,3 +79,4 @@ void loop() {
   reset();
   delay(1);
 }
+
